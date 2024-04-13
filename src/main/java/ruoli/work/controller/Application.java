874eea9a69b39070cf246fdc86546b558772e9d8
Ruoli <ruoli@outlook.com>
2024-04-13 23:14:28 +0800

    clear

---
+++ src/main/java/ruoli/work/controller/Application.java
@@ -0,0 +1,13 @@
+package ruoli.work.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(value = "ruoli.work")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
